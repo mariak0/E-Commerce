0b534b477948ae708c95a7eac557c459656c0d4a
Maria Kouri <mariakouri@MacBook---Maria.local>
2024-06-05 11:07:16 +0300

    Initial commit

---
+++ wp-content/mu-plugins/aios-firewall-loader.php
@@ -0,0 +1,6 @@
+<?php
+// Begin AIOWPSEC Firewall
+if (file_exists('/Applications/XAMPP/xamppfiles/htdocs/wordpressonmac/aios-bootstrap.php')) {
+	include_once('/Applications/XAMPP/xamppfiles/htdocs/wordpressonmac/aios-bootstrap.php');
+}
+// End AIOWPSEC Firewall
